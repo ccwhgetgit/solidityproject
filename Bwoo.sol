20c4ab342d732e728831cda7fc914cc033a10462
Bryanwoo <66771259+Buwoo@users.noreply.github.com>
2020-12-10 00:18:46 +0800

    Update Bwoo.sol
    Change of ether to tokens, use of tokens to bet on a certain number (participate function) and can activate the gameplay function if there are a certain number of people (5) participating. 
    Smart contract auto self destructs and issues relevant payouts upon completion of 1 round of gameplay

--- Bwoo.sol
+++ Bwoo.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.7.5;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
+//Part 1: Creation of the Tokenisation system 
+
 contract Tokencreation{
     struct Player{
         uint totaltokens;
@@ -12,8 +14,10 @@ contract Tokencreation{
     
     mapping(address => Player) playerinfo;
     
+    //Globallist includes all users who have changed x amount of ether for y amount of tokens. May or may not be participating in the game 
     address payable[] globallist;
     
+    //Function ethertotoken is to change ether to tokens 
     function EthertoToken() public payable{
         require (msg.value>=1 ether, "Need to change more than 1 Ether");
         require (msg.sender.balance>= msg.value/1 ether,"Not enough funds to convert to Tokens");
@@ -22,10 +26,12 @@ contract Tokencreation{
 
     }
     
+    //Function to check how many tokens the player has in his/her own "mini" token wallet  
     function Playertokens() public view returns (uint){
         return playerinfo[msg.sender].totaltokens;
     }
     
+    //Just a debugging function to see total ethers in the smart contract (Dont mix up with total tokens bet in the tokenpot) 
     /*   
     function getBalance() public view returns (uint){
         return address(this).balance;
@@ -33,6 +39,8 @@ contract Tokencreation{
     */
 
 }
+
+//Part 2: Actual implementation of the HollyRollyPolly Game 
 
 contract HollyRollyPolly is Tokencreation{
     using SafeMath for uint;
@@ -42,13 +50,18 @@ contract HollyRollyPolly is Tokencreation{
     address payable[] playerlist;
     address payable[] winnerlist;
     uint tokenpot;
+    uint counter=1;
     
     constructor() public{
         owner=msg.sender;
     }
     
-  
+    function rndGenerate(uint mod) internal returns(uint){
+        counter++; //Cannot be view since we are modifying sth in the function. 
+        return uint(keccak256(abi.encodePacked(counter, block.timestamp, block.difficulty, msg.sender))) % mod;
+    }
     
+    //Function for participation in the game. Player pushed to participating array
     function participate(uint _nroftokens, uint _numselected) public payable {
         require(_nroftokens>0 ,"Need to bet more than 0");
         require(!playerinfo[msg.sender].userexists,"Player is already inside the game");
@@ -62,6 +75,7 @@ contract HollyRollyPolly is Tokencreation{
         playerinfo[msg.sender].userexists=true;
     }
     
+    //numplayers and potsize are mainly debugging features. Can change out of public view subsequently/remove altogether
     function numberofplayers() public view returns (uint){
         return playerlist.length;
     }
@@ -71,8 +85,8 @@ contract HollyRollyPolly is Tokencreation{
     }
     
     function gameplay() public payable{
-        require(playerlist.length>=1,"Not enough players");
-        uint answer = (696969*block.number+block.timestamp)%6 + 1;
+        require(playerlist.length>=5,"Not enough players");
+        uint answer = rndGenerate(6) + 1;
         
         uint winnertottokens;
         for (uint i=0; i<playerlist.length;i++){
